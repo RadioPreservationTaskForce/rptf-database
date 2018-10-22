0bd1ee89e56e0db69d344784ebc36f3ca52670fb
Mark A. Matienzo <matienzo@stanford.edu>
2018-10-22 10:00:01 -0700

    Add link to finding aids

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -83,7 +83,7 @@ class CatalogController < ApplicationController
        #:years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
        #:years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
     #}
-    
+
     config.add_facet_field 'collectionContentTypesFacet', label: 'Content type', limit: 25
     config.add_facet_field 'collectionFormatsFacet', label: 'Format', limit: 25
     config.add_facet_field 'collectionGenresFacet', label: 'Genre', limit: 25
@@ -114,7 +114,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'collectionFormats_txt', label: 'Formats', link_to_search: 'collectionFormatsFacet'
     config.add_index_field 'collectionExtent_txt', label: 'Extent'
     config.add_index_field 'collectionOwnerName_txt', label: 'Repository/Collector'
-    config.add_index_field 'collectionFindingAidUrl_txt', label: 'Online finding aid'
+    config.add_index_field 'collectionFindingAidUrl_txt', helper_method: :link_to_finding_aid, label: 'Online finding aid'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
@@ -132,7 +132,7 @@ class CatalogController < ApplicationController
     #config.add_show_field 'published_vern_display', label: 'Published'
     #config.add_show_field 'lc_callnum_display', label: 'Call number'
     #config.add_show_field 'isbn_t', label: 'ISBN'
-    
+
     config.add_show_field 'collectionDescription_txt', label: 'Description'
     config.add_show_field 'collectionCreators_txt', label: 'Creators'
     config.add_show_field 'collectionDates_txt', label: 'Dates'
@@ -149,22 +149,22 @@ class CatalogController < ApplicationController
     config.add_show_field 'collectionIndexingTerms_txt', label: 'Indexing terms'
     config.add_show_field 'collectionLanguages_txt', label: 'Languages'
     config.add_show_field 'collectionGenres_txt', label: 'Genres'
-    
+
     config.add_show_field 'collectionAccessStatement_txt', label: 'Access statement'
     config.add_show_field 'collectionUsageStatement_txt', label: 'Usage statement'
-    
+
     config.add_show_field 'collectionInventoryDescription_txt', label: 'Inventory description'
     config.add_show_field 'collectionSupportingDocumentationDescription_txt', label: 'Supporting documentation'
     config.add_show_field 'collectionHistoricalRelevance_txt', label: 'Historical relevance'
     config.add_show_field 'collectionCondition_txt', label: 'Condition note'
     config.add_show_field 'collectionNotes_txt', label: 'Notes'
-    
+
     config.add_show_field 'collectionWebsiteUrl_txt', label: 'Website URL'
-    config.add_show_field 'collectionFindingAidUrl_txt', label: 'Online finding aid'
+    config.add_show_field 'collectionFindingAidUrl_txt', helper_method: :link_to_finding_aid, label: 'Online finding aid'
     config.add_show_field 'collectionOclcNumber_txt', label: 'OCLC record'
     config.add_show_field 'collectionCatalogUrl_txt', label: 'Local catalog record'
-    
-    
+
+
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
